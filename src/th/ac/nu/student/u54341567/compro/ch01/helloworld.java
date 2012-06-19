2e25b84b05539111c6716ba647b0a90beadc57a2
nozhi <n-bc@msn.com>
2012-06-19 14:22:50 +0700

    เพิ่ม programming is fun

--- src/th/ac/nu/student/u54341567/compro/ch01/helloworld.java
+++ src/th/ac/nu/student/u54341567/compro/ch01/helloworld.java
@@ -1,13 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package th.ac.nu.student.u54341567.compro.ch01;
 
-/**
- *
- * @author Administrator
- */
-public class helloworld {
-    
+package th.ac.nu.student.u54341567.compro.ch01;
+public class helloworld{
+public static void main(String[] args){
+
+int h;
+System.out.println();
+        
+        }
 }
