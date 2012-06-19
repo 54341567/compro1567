a0fc5a4e71baf859882d0058e7644ec4cc1ed686
nozhi <n-bc@msn.com>
2012-06-19 14:18:30 +0700

    สร้าง

---
+++ src/th/ac/nu/student/u54341567/compro/ch01/helloworld.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341567.compro.ch01;
+
+/**
+ *
+ * @author Administrator
+ */
+public class helloworld {
+    
+}
